9d2e9a57d8fcc206d8e02d8b491e464d1a464be5
Martin Czygan <martin.czygan@gmail.com>
2022-12-14 14:26:57 +0100

    cidump

--- certinfo/setup.py
+++ certinfo/setup.py
@@ -15,4 +15,8 @@ setup(
     version="1.0",
     description="Wrapper around certinfo",
     ext_modules=[certinfo],
+    scripts=["cidump.py"],
+    install_requires=[
+        "termcolor",
+    ],
 )
